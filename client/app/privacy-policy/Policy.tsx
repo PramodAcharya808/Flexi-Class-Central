da2c8a8034a5bb356cea1f610901c54331b694a1
charu <charupsharma5@gmail.com>
2024-05-17 12:36:57 +0530

    policy page modified

--- client/app/privacy-policy/Policy.tsx
+++ client/app/privacy-policy/Policy.tsx
@@ -7,15 +7,15 @@ const Policy = (props: Props) => {
   return (
     <div>
       <div className={"w-[95%] 800px:w-[92%] m-auto py-2  text-black dark:text-white px-3"}>
-        <h1 className={`${styles.title} !text-start pt-2`}>
+        <h1 className={`${styles.title} !text-start pt-2 text-gradient font-semibold`} >
           Platform Terms and Condition
         </h1>
         <ul style={{ listStyle: "unset", marginLeft: "15px" }}>
           <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
-            <h2 className={`${styles.subtitle}`}>Last updated: February 04, 2024</h2>
+            <h2 className={`${styles.subtitle} font-semibold`}>Last updated: May 17, 2024</h2>
             <p>&#8226; This Privacy Policy includes important information about your personal data and we encourage you to read it carefully.</p>
             <p>&#8226; We collect information about you during the checkout process in our store.</p><br />
-            <p className={`${styles.title} !text-start pt-2`}>What we collect and store</p>
+            <p className={`${styles.title} !text-start pt-2 text-gradient font-semibold`}>What we collect and store</p>
             <p>While you visit our site, we’ll track the following:</p>
             <p>&#8226; Products you’ve viewed: we’ll use this to, for example, show you products you’ve recently viewed</p>
             <p>&#8226; Location, IP address, and browser type: we’ll use this for purposes like estimating taxes and shipping</p>
@@ -32,16 +32,16 @@ const Policy = (props: Props) => {
 
             <p>We generally store information about you for as long as we need the information for the purposes we collect and use it, and we are not legally required to continue to keep it. For example, we will store order information for three years for tax and accounting purposes. This includes your name, email address, and company name.</p>
             <p>We will also store comments or reviews if you choose to leave them.</p><br />
-            <p className={`${styles.title} !text-start pt-2`}>Who on our team has access</p>
+            <p className={`${styles.title} !text-start pt-2 text-gradient font-semibold`}>Who on our team has access</p>
             <p>Members of our team have access to the information you provide us. For example, both Administrators and Shop Managers can access the following:</p>
             <p>&#8226; Order information like what was purchased, when it was purchased and where it should be sent, and</p>
             <p>&#8226; Customer information like your name, email address, and company name.</p>
 
             <br />
-            <p className={`${styles.title} !text-start pt-2`}>Payments</p>
-            <p>Stripe</p>
+            <p className={`${styles.title} !text-start pt-2 text-gradient font-semibold`}>Payments</p>
+            <p className="font-semibold">Stripe</p>
             <p>We accept Credit card payments through Stripe only in EU region. The payment interface is managed by Stripe and we do not store credit card data on our website.</p>
-            <p>Please see the <a className={`${styles.input}`} href="https://www.paypal.com/us/legalhub/privacy-full">Stripe Privacy Policy</a> for more details.</p>
+            <p>Please see the <a className={`${styles.input} font-semibold`} target="_blank" href="https://www.paypal.com/us/legalhub/privacy-full">Stripe Privacy Policy</a> for more details.</p>
 
           </p>
           <p className="py-2 ml-[-15px] text-[16px] font-Poppins leading-8 whitespace-pre-line">
