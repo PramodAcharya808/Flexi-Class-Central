bc18a2ce6134521338aa35349950903121572091
charu <charupsharma5@gmail.com>
2024-05-17 14:15:28 +0530

    admin-team-page.tsx modified

--- client/app/admin/team/page.tsx
+++ client/app/admin/team/page.tsx
@@ -13,8 +13,8 @@ const page = (props: Props) => {
     <div>
       <AdminProtected>
         <Heading
-          title="Vishwa Cloud Lab - Admin"
-          description="Vishwa Cloud Lab is a platform for learners to learn and get help from leading tech instructors"
+          title="Flexi Class - Admin"
+          description="Flexi Class is a platform for learners to learn and get help from leading tech instructors"
           keywords="Programming,MERN,Redux,Machine Learning, security, linux, cloud computing, cloud administration, cloud security,cyber security"
         />
         <div className="flex h-screen">
