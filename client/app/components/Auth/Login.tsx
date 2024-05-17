ae2b62d42d61f707aa3d856b00e32322767779e3
charu <charupsharma5@gmail.com>
2024-05-17 14:30:23 +0530

    components-Admin-Auth-Login.tsx modified

--- client/app/components/Auth/Login.tsx
+++ client/app/components/Auth/Login.tsx
@@ -126,7 +126,7 @@ const Login: FC<Props> = ({ setRoute, setOpen, refetch }) => {
           >
             Sign up
           </span>
-            <h5 className="text-center pt-4 font-Poppins text-[14px] text-black dark:text-white">VCL v1.0.0</h5>
+            <h5 className="text-center pt-4 font-Poppins text-[14px] text-black dark:text-white">Flexi Class v1.0.0</h5>
         </h5>
       </form>
       <br />
